8c5f7ffd9d79d1141d626d2b9390d9c525298013
Harun Karacuban <karacuban@sipgate.de>
2023-01-05 13:09:59 +0100

    :sparkles: add functionallity to refresh tokens and get userinfo
    Co-authored-by: Malte Sehmer <sehmer@sipgate.de>
    Co-authored-by: Mathias Bethge <bethge@sipgate.de>
    Co-authored-by: Moritz Konschack <konschack@sipgate.de>

--- src/router.php
+++ src/router.php
@@ -8,48 +8,19 @@ use Steampixel\Route;
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__."/..");
 $dotenv->load();
 
-
-Route::add('/', function() {
-
-    $clientId = $_ENV['clientId'];
-    $redirectUri = $_ENV['redirectUri'];
-    $oauthScope = $_ENV['oauthScope'];
-    $sessionState = uniqid();
-
-    //   const queryString = querystring.stringify(params);
-    $params = array(
-        "client_id" => $clientId,
-        "redirect_uri" => $redirectUri,
-        "scope" => $oauthScope,
-        "response_type" => "code",
-        "state" => $sessionState,
-    );
-
-    $queryString = http_build_query($params);
-    $apiAuthUrl = $_ENV['authUrl']."?".$queryString;
-
-    print("Please open the following URL in your browser: \n" . "<a href='".$apiAuthUrl."'> Link </a>");
-
-});
-
-Route::add('/oauth', function(){
-
-    $receivedState = $_GET['state'];
-    $authorizationCode = $_GET['code'];
-    $tokenURL = $_ENV['tokenUrl'];
-
-    $params =  array(
+function retrieveTokens(){
+    $params =  [
         "client_id" =>  $_ENV['clientId'],
         "client_secret" => $_ENV['clientSecret'],
         "redirect_uri" => $_ENV['redirectUri'],
-        "code" => $authorizationCode,
+        "code" =>  $_GET['code'],
         "grant_type" => 'authorization_code'
-    );
+    ];
 
     $params_string = http_build_query($params);
     $ch = curl_init();
 
-    curl_setopt($ch,CURLOPT_URL, $tokenURL);
+    curl_setopt($ch,CURLOPT_URL, $_ENV['tokenUrl']);
     curl_setopt($ch,CURLOPT_POST, true);
     curl_setopt($ch,CURLOPT_POSTFIELDS, $params_string);
 
@@ -60,8 +31,86 @@ Route::add('/oauth', function(){
     $access_token = $decoded_result->access_token;
     $refresh_token = $decoded_result->refresh_token;
 
+    return [
+        'access_token'=>$access_token,
+        'refresh_token'=>$refresh_token
+    ];
+}
+
+function refreshTokens($refresh_token){
+    $params =  [
+        "client_id" =>  $_ENV['clientId'],
+        "client_secret" => $_ENV['clientSecret'],
+        "refresh_token" => $refresh_token,
+        "grant_type" => 'refresh_token'
+    ];
+
+    $params_string = http_build_query($params);
+    $ch = curl_init();
+
+    curl_setopt($ch,CURLOPT_URL, $_ENV['tokenUrl']);
+    curl_setopt($ch,CURLOPT_POST, true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS, $params_string);
+
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+
+    $result = curl_exec($ch);
+    $decoded_result = json_decode($result);
+
+    $access_token = $decoded_result->access_token;
+    $refresh_token = $decoded_result->refresh_token;
+
+    return [
+        'access_token'=>$access_token,
+        'refresh_token'=>$refresh_token
+    ];
+}
+
+function userInfo($accessToken) {
+    $options = array(
+        'http' => array(
+            'header'  => "Authorization: Bearer $accessToken\r\n",
+        ),
+    );
+    $context  = stream_context_create($options);
+    $response = file_get_contents($_ENV['testApiEndpoint'], false, $context);
+    echo "Response: ";
+    echo $response;
+    return $response;
+}
+
+
+Route::add('/', function() {
+
+    $clientId = $_ENV['clientId'];
+    $redirectUri = $_ENV['redirectUri'];
+    $oauthScope = $_ENV['oauthScope'];
+    $sessionState = uniqid();
+
+    //   const queryString = querystring.stringify(params);
+    $params = [
+        "client_id" => $clientId,
+        "redirect_uri" => $redirectUri,
+        "scope" => $oauthScope,
+        "response_type" => "code",
+        "state" => $sessionState,
+    ];
+
+    $queryString = http_build_query($params);
+    $apiAuthUrl = $_ENV['authUrl']."?".$queryString;
+
+    print("Please open the following URL in your browser: \n" . "<a href='".$apiAuthUrl."'> Link </a>");
+
+});
+
+//retrieveToken
+Route::add('/oauth', function(){
+    define("retrievedTokens", retrieveTokens());
+    $access_token = retrievedTokens['access_token'];
+    $refresh_token = retrievedTokens['refresh_token'];
+    echo "Access Token: ";
     echo $access_token;
-    echo $refresh_token;
+    userInfo($access_token);
 });
 
 Route::run('/');
