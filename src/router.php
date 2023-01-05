b9ff34fb4e70b5ebc9c959854c7a60bff624d998
Mathias Bethge <bethge@sipgate.de>
2023-01-05 16:02:52 +0100

    :sparkles: implement oAuth flow
    Co-authored-by: Malte Sehmer <sehmer@sipgate.de>
    Co-authored-by: Moritz Konschack <konschack@sipgate.de>

--- src/router.php
+++ src/router.php
@@ -5,54 +5,63 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Sipgate\Io\Example\OAuth\OAuthHandler;
 use Steampixel\Route;
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__."/..");
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
 $dotenv->load();
 
-function retrieveTokens(){
-    $params =  [
-        "client_id" =>  $_ENV['clientId'],
+if(!isset($_COOKIE['io_session_identifier'])) {
+    setcookie('io_session_identifier', uniqid());
+}
+
+$io_session_identifier = $_COOKIE['io_session_identifier'];
+
+function retrieveTokens()
+{
+    $params = [
+        "client_id" => $_ENV['clientId'],
         "client_secret" => $_ENV['clientSecret'],
         "redirect_uri" => $_ENV['redirectUri'],
-        "code" =>  $_GET['code'],
-        "grant_type" => 'authorization_code'
+        "code" => $_GET['code'],
+        "grant_type" => 'authorization_code',
     ];
 
     $params_string = http_build_query($params);
     $ch = curl_init();
 
-    curl_setopt($ch,CURLOPT_URL, $_ENV['tokenUrl']);
-    curl_setopt($ch,CURLOPT_POST, true);
-    curl_setopt($ch,CURLOPT_POSTFIELDS, $params_string);
+    curl_setopt($ch, CURLOPT_URL, $_ENV['tokenUrl']);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $params_string);
 
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
     $result = curl_exec($ch);
     $decoded_result = json_decode($result);
     $access_token = $decoded_result->access_token;
     $refresh_token = $decoded_result->refresh_token;
 
+
     return [
-        'access_token'=>$access_token,
-        'refresh_token'=>$refresh_token
+        'access_token' => $access_token,
+        'refresh_token' => $refresh_token,
     ];
 }
 
-function refreshTokens($refresh_token){
-    $params =  [
-        "client_id" =>  $_ENV['clientId'],
+function refreshTokens($refresh_token)
+{
+    $params = [
+        "client_id" => $_ENV['clientId'],
         "client_secret" => $_ENV['clientSecret'],
         "refresh_token" => $refresh_token,
-        "grant_type" => 'refresh_token'
+        "grant_type" => 'refresh_token',
     ];
 
     $params_string = http_build_query($params);
     $ch = curl_init();
 
-    curl_setopt($ch,CURLOPT_URL, $_ENV['tokenUrl']);
-    curl_setopt($ch,CURLOPT_POST, true);
-    curl_setopt($ch,CURLOPT_POSTFIELDS, $params_string);
+    curl_setopt($ch, CURLOPT_URL, $_ENV['tokenUrl']);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $params_string);
 
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
     $result = curl_exec($ch);
     $decoded_result = json_decode($result);
@@ -61,59 +70,78 @@ function refreshTokens($refresh_token){
     $refresh_token = $decoded_result->refresh_token;
 
     return [
-        'access_token'=>$access_token,
-        'refresh_token'=>$refresh_token
+        'access_token' => $access_token,
+        'refresh_token' => $refresh_token
     ];
 }
 
-function userInfo($accessToken) {
+function userInfo($accessToken)
+{
     $options = array(
         'http' => array(
-            'header'  => "Authorization: Bearer $accessToken\r\n",
+            'header' => "Authorization: Bearer $accessToken\r\n",
         ),
     );
-    $context  = stream_context_create($options);
-    $response = file_get_contents($_ENV['testApiEndpoint'], false, $context);
-    echo "Response: ";
-    echo $response;
-    return $response;
+    $context = stream_context_create($options);
+    return file_get_contents($_ENV['testApiEndpoint'], false, $context);
 }
 
-
-Route::add('/', function() {
+Route::add('/', function () {
 
     $clientId = $_ENV['clientId'];
     $redirectUri = $_ENV['redirectUri'];
     $oauthScope = $_ENV['oauthScope'];
-    $sessionState = uniqid();
 
-    //   const queryString = querystring.stringify(params);
     $params = [
         "client_id" => $clientId,
         "redirect_uri" => $redirectUri,
         "scope" => $oauthScope,
         "response_type" => "code",
-        "state" => $sessionState,
+        "state" => $GLOBALS['io_session_identifier']
     ];
 
     $queryString = http_build_query($params);
-    $apiAuthUrl = $_ENV['authUrl']."?".$queryString;
+    $apiAuthUrl = $_ENV['authUrl'] . "?" . $queryString;
 
-    print("Please open the following URL in your browser: \n" . "<a href='".$apiAuthUrl."'> Link </a>");
+    print("Please open the following URL in your browser: \n" . "<a href='" . $apiAuthUrl . "'> Link </a>");
 
 });
 
 //retrieveToken
-Route::add('/oauth', function(){
+Route::add('/oauth', function () {
+
+    $query_session_identifier = $_GET['state'];
+
+    if ($query_session_identifier != $GLOBALS['io_session_identifier']) {
+        echo 'State in the callback does not match the state in the original request.';
+        return;
+    }
+    // Get access token
+    printf("Getting tokens...");
     define("retrievedTokens", retrieveTokens());
     $access_token = retrievedTokens['access_token'];
-    $refresh_token = retrievedTokens['refresh_token'];
-    echo "Access Token: ";
-    echo $access_token;
-    userInfo($access_token);
+    echo "<p>Received new tokens:</p>";
+    var_dump(retrievedTokens);
+
+
+        // Get user information
+        echo "<p>Getting User Information...</p>";
+        define("userInformations", userInfo($access_token));
+        echo userInformations;
+
+        // Refresh tokens
+        echo "<p>Refreshing Tokens...</p>";
+        define("refreshTokens", refreshTokens(retrievedTokens['refresh_token']));
+        $refreshed_access_token = refreshTokens['access_token'];
+        echo "Received new Tokens:" . $refreshed_access_token;
+
+        // Get user information using the refreshed accessToken
+        echo "<p>Getting user Information with refreshed Tokens...</p>";
+        define("userInformationWithRefreshedToken", userInfo($refreshed_access_token));
+        echo userInformationWithRefreshedToken;
+
 });
 
 Route::run('/');
-
 
 ?>
